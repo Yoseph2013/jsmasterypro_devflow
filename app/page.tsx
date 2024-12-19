9fa0a8384306da64332a107ab1d169bfaf684e0a
Yoseph <yosephalpha2023@gmail.com>
2024-12-19 15:41:25 -0800

    Tailwindcss & Font Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-white">Welcome to Next.js World</h1>
+    <h1 className="font-inter text-3xl font-black text-light-500">
+      Welcome to Next.js World
+    </h1>
   );
 };
 export default Home;
